51a648e2855b5bc4527df8ea09915ab779427b23
bertbook <69720719+bertbook@users.noreply.github.com>
2020-08-20 20:41:05 +0530

    Update TextSummarization.py

--- Chapter6/TextSummarization/TextSummarization.py
+++ Chapter6/TextSummarization/TextSummarization.py
@@ -6,13 +6,11 @@ def text_summary(text):
 
 
 """ 
-text = "OpenGenus Foundation is an open-source non-profit organization with the aim to enable people 
-    to work offline for a longer stretch, reduce the time spent on searching by exploiting the fact that 
-    almost 90% of the searches are 	same for every generation and to make programming more accessible.
-    OpenGenus is all about positivity and innovation.Over 1000 people have contributed to our missions 
-    and joined our family. We have been sponsored by three 	great companies namely Discourse, 
-    GitHub and DigitalOcean. We run one of the most popular Internship program and 	
-    open-source projects and have made a positive impact over people's life."
+text = "Machine learning (ML) is the study of computer algorithms that improve automatically through experience.
+It is seen as a subset of artificial intelligence. Machine learning algorithms build a mathematical model based on sample data,
+known as "training data", in order to make predictions or decisions without being explicitly programmed to do so.
+Machine learning algorithms are used in a wide variety of applications, such as email filtering and computer vision,
+where it is difficult or infeasible to develop conventional algorithms to perform the needed tasks."
 
 print(text_summary(text))
 
