8abfc7397ead6b28ba2b7c8c2a2304e963797888
bertbook <69720719+bertbook@users.noreply.github.com>
2020-08-20 20:37:34 +0530

    Update QA_Deepplavlov.py

--- Chapter5/DeeppavlovQASystem/QA_Deepplavlov.py
+++ Chapter5/DeeppavlovQASystem/QA_Deepplavlov.py
@@ -5,9 +5,9 @@ def qa_deeppavlov(question, context):
     result = model([context], [question])
     return result
 
-context="Super Bowl 50 was an American football game to determine the champion of the National Football League (NFL) for the 2015 season. The American Football Conference (AFC) champion Denver Broncos defeated the National Football Conference (NFC) champion Carolina Panthers 24–10 to earn their third Super Bowl title. The game was played on February 7, 2016, at Levi's Stadium in the San Francisco Bay Area at Santa Clara, California. As this was the 50th Super Bowl, the league emphasized the 'golden anniversary' with various gold-themed initiatives, as well as temporarily suspending the tradition of naming each Super Bowl game with Roman numerals (under which the game would have been known as 'Super Bowl L'), so that the logo could prominently feature the Arabic numerals 50."
+context = " In 1888, The Football League was founded in England, becoming the first of many professional football competitions. During the 20th century, several of the various kinds of football grew to become some of the most popular team sports in the world."
 
-question="Which NFL team represented the AFC at Super Bowl 50?"
+question = "In which year the Football league was founded?”
 
 answers = qa_deeppavlov(context, question)
 print(answers)
